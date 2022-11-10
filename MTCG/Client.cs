723eded939d12a8bd8249ec5c889a2228d5ba189
random-dash <abdelhady@gmx.at>
2022-11-10 23:56:58 +0100

    asd

---
+++ MTCG/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Thread.Sleep(2000);
+
+            using TcpClient client = new TcpClient("localhost", 8000);
+            using StreamReader reader = new StreamReader(client.GetStream());
+            Console.WriteLine($"{ reader.ReadLine() }{ Environment.NewLine }{ reader.ReadLine() }");
+            using StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+
+            string input = null;
+            while((input = Console.ReadLine()) != "quit")
+            {
+                writer.WriteLine(input);
+            }
+            writer.WriteLine("quit");
+        }
+    }
+}
---
+++ MTCG/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+//Server with socket programming and multithreading 
+namespace Serverr
+{
+
+    public class Server
+    {
+        public static async Task Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 8000);
+            listener.Start(5);
+
+            Console.CancelKeyPress += (sender, e) => Environment.Exit(0);
+
+            while (true)
+            {
+                try
+                {
+                    var socket = await listener.AcceptTcpClientAsync();
+                    using var writer = new StreamWriter(socket.GetStream()) { AutoFlush = true };
+                    writer.WriteLine("Welcome to the Arena!");
+
+                    using var reader = new StreamReader(socket.GetStream());
+                    string message;
+                    do
+                    {
+                        message = await reader.ReadLineAsync();
+                        Console.WriteLine(message);
+                        await writer.WriteLineAsync(message);
+                        while (message != null) ;
+                    } while (message != "quit");
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("error occured: " + exc.Message);
+                }
+            }
+        }
+    }
+
+    
+
+}
