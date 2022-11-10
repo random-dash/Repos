dfc32a927c9e467785a82c70a5d05039e781a885
random-dash <abdelhady@gmx.at>
2022-11-10 08:10:01 +0100

    0

--- MTCG/Program.cs
+++ MTCG/Program.cs
@@ -9,18 +9,17 @@ namespace MTCG
         public static void Main(string[] args)
         {
             CardManager cardManager = new CardManager();
-            cardManager.uploadAllCards();
+            //cardManager.uploadAllCards();
             cardManager.LoadCardsFromDb();
-            cardManager.PrintCards();
             //cardManager.PrintCards();
-            /*User player = new User();
+            User player = new User();
             UI game = new UI();
             game.Menu(player);
             Console.WriteLine("user: " + player.username);
             Console.WriteLine("userid: " + player.userid);
             Console.WriteLine("coins: " + player.Coins);
             Console.WriteLine("points: " + player.Points);
-            player.BuyPack(cardManager.GetCards());*/
+            player.BuyPack(cardManager.GetCards());
             //player.BuyPack(cardManager.AllCards());
             //player.Arena(player.Stack);
             //Console.WriteLine(player.Stack.Monsters() + " " + player.Stack.Spells());
